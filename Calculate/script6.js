315596d0999432de4f551b6e41aa6a75696a13ae
Snehdipmote <53633645+Snehdipmote@users.noreply.github.com>
2023-03-07 23:40:04 +0530

    Create script6.js

---
+++ Calculate/script6.js
@@ -0,0 +1,31 @@
+// program for a simple calculator
+let result;
+function calculate(number1,number2,operator)
+{
+switch(operator) {
+    case '+':
+        result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
+    case '-':
+        result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+    case '*':
+        result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+    case '/':
+        result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+
+    default:
+        console.log('Invalid operator');
+        break;
+}
+}
+calculate(9,4,"+");
+calculate(9,4,"-");
+calculate(9,4,"*");
+calculate(9,4,"/");
