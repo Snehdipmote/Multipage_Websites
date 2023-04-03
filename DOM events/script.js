833d28a6c2e624a21b468eebdc77a2dda87ac1a1
Snehdipmote <53633645+Snehdipmote@users.noreply.github.com>
2023-04-04 01:48:15 +0530

    Create script.js

---
+++ DOM events/script.js
@@ -0,0 +1,45 @@
+let c1=document.getElementById("doubleclick");
+
+c1.addEventListener("dblclick",()=>{
+    c1.innerText="Clicked twice";
+    c1.style.backgroundColor="Orange";
+    alert(
+        "Double click event is working properly)"
+      );
+})
+
+let c2=document.getElementById("mover");
+
+c2.addEventListener("mouseover",()=>{
+    alert(
+        "you entered in Restricted Area 🙅 (mouse over event is working properly)"
+      );
+})
+
+let c3=document.getElementById("mout");
+
+c3.addEventListener("mouseout",()=>{
+    alert("you left the safe zone 🙅 (mouse out event working properly)");
+})
+
+let c4=document.getElementById("keypress");
+
+c4.addEventListener('keypress', function(e){
+    display.innerText ="you have Pressed " + e.key
+})
+
+let c5=document.getElementById("display2");
+// adding a keydown event listener to the document
+// key down shows till when key is pressed
+document.addEventListener("keydown", function (e) {
+    c5.style.color = "red";
+    c5.innerText = e.key + " is keyDown";
+  });
+  
+// adding a keyup event listener to the document
+//   keyup is shown when key is released
+  document.addEventListener("keyup", function (e) {
+    c5.style.color = "green";
+    c5.innerText = e.key + " is keyUp";
+  });
+
